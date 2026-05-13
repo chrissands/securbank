f623f1c13406860882cd3b6003060b80466ca438
Chris Sands <chris.k.sands@gmail.com>
2026-05-13 08:48:50 -0400

    Update AEM endpoint URLs in endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
+  return 'https://publish-p194952-e2026230.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p149891-e1546481.adobeaemcloud.com';
+  return 'https://author-p194952-e2026230.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
